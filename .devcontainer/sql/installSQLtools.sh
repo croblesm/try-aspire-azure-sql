b32d4c3031df88e0e19c2f19b9672aca0e9a01f6
Carlos Robles <contact@croblesm.com>
2024-05-03 04:05:58 +0000

    Updating to SQL Database project

--- .devcontainer/sql/installSQLtools.sh
+++ .devcontainer/sql/installSQLtools.sh
@@ -7,9 +7,10 @@ sudo apt-get install sqlcmd
 echo "Go-SQLCmd installed."
 
 echo "Installing Sqlpackage ..."
-curl -sSL -o sqlpackage.zip "https://aka.ms/sqlpackage-linux"
-mkdir /opt/sqlpackage
-unzip sqlpackage.zip -d /opt/sqlpackage 
-rm sqlpackage.zip
-chmod a+x /opt/sqlpackage/sqlpackage
+# curl -sSL -o sqlpackage.zip "https://aka.ms/sqlpackage-linux"
+# mkdir /opt/sqlpackage
+# unzip sqlpackage.zip -d /opt/sqlpackage 
+# rm sqlpackage.zip
+# chmod a+x /opt/sqlpackage/sqlpackage
+dotnet tool install --global microsoft.sqlpackage
 echo "Sqlpackage installed."
---
+++ database/Library/Tables/LeTable.sql
@@ -0,0 +1,4 @@
+CREATE TABLE [dbo].[LeTable]
+(
+  [Id] INT NOT NULL PRIMARY KEY
+)
